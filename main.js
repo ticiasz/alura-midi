b47425e10ec9d227e3cc0cc2ea1df108fc77cd22
Letícia Matheus <let.matheus19@hotmail.com>
2023-08-03 17:09:21 -0300

    Criando a função tocaSom e adicionando ela onclick para um botão

---
+++ main.js
@@ -0,0 +1,7 @@
+function tocaSomPom (){
+    document.querySelector('#som_tecla_pom').play();
+}
+
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
+// onclick é um atributo, portanto devemos atribuir um valor a ele utilizando o =
+//a função está sem o () pois se não ela seria executada imediatamente, não guardada dentro do onclick
