e540ea93e0d87bb7ebb3be220dc3a903cb35d269
Letícia Matheus <let.matheus19@hotmail.com>
2023-08-04 17:07:41 -0300

    Criando uma referência para a lista de teclas

--- main.js
+++ main.js
@@ -2,6 +2,10 @@ function tocaSomPom (){
     document.querySelector('#som_tecla_pom').play();
 }
 
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
+//document.querySelector('.tecla_pom').onclick = tocaSomPom;
 // onclick é um atributo, portanto devemos atribuir um valor a ele utilizando o =
 //a função está sem o () pois se não ela seria executada imediatamente, não guardada dentro do onclick
+
+const listaDeTeclas = document.querySelectorAll('.tecla')
+
+listaDeTeclas[0].onclick = tocaSomPom;
