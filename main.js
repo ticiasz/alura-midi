f771c5b7b2c9625fa0e4d1a2bf20bb49bf21cbd1
Letícia Matheus <let.matheus19@hotmail.com>
2023-08-04 17:23:32 -0300

    Criando um while

--- main.js
+++ main.js
@@ -8,4 +8,14 @@ function tocaSomPom (){
 
 const listaDeTeclas = document.querySelectorAll('.tecla')
 
-listaDeTeclas[0].onclick = tocaSomPom;
+// listaDeTeclas[0].onclick = tocaSomPom;
+
+let contador = 0;
+
+while (contador < listaDeTeclas.length){
+    listaDeTeclas[contador].onclick = tocaSomPom;
+
+    contador = contador + 1;
+
+    console.log(contador);
+}
